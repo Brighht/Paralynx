37f112263fbfd496930f73fd33a3901535224c26
Brighht <bright.andohh@gmail.com>
2025-05-25 19:22:01 -0500

    Refactor Task structure: add created_at timestamp and constructor

--- include/Task.h
+++ include/Task.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <functional>
 #include <string>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 struct Task {
     string id;
     function<void()> work;
+    high_resolution_clock::time_point created_at = high_resolution_clock::now();
+
+    Task(const string& task_id, function<void()> task_work)
+        : id(task_id), work(std::move(task_work)) {}
 };
