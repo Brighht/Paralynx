8a7b09c65ce7a351f1d27ab8f6d3040d1fe3d0e1
Brighht <bright.andohh@gmail.com>
2025-05-26 20:09:14 -0500

    Add TaskQueue class with push, pop, and isEmpty methods

---
+++ include/TaskQueue.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <queue>
+#include <mutex>
+#include "Task.h"
+
+using namespace std;
+
+class TaskQueue {
+    private:
+        queue<Task> tasks;
+        mutex mtx;
+    
+    public:
+        // Add A Task to the queue
+        void push(const Task& task);
+
+        //Remove and return the next task from the queue(copy for now)
+        Task pop();
+
+        bool isEmpty() const;
+};
